f6b9e36df32714050ee1e9138d28ff53dcba0c36
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-07-24 04:20:07 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+//store person name and print it in title lower case and upper case
+var name1 =("Eric");
+//to upper case
+var uppercasename = ("upper case name");
+console.log("uppercasename");
+
+//to lowercase
+
+var lowercasename = ("lower case name");
+console.log("lowercasename");
+//to title case
+
+var titlecasename = ("title case name");
+console.log("titlecasename");
